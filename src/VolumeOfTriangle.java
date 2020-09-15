558f81add60a05d356d58eae64f93ad7c81738ae
AsHuOvSo <asta@avra.dk>
2020-09-15 12:35:43 +0200

    Works with full numbers, but not with decimals

--- src/VolumeOfTriangle.java
+++ src/VolumeOfTriangle.java
@@ -1,9 +1,21 @@
-//Import scanner library
+//Import scanner and math library
 import java.util.Scanner;
+import java.lang.Math;
 
 public class VolumeOfTriangle {
     //Start of main code
     public static void main(String[] args) {
-        
+        //write starting line
+        System.out.println("Enter length of the Equilateral triangle:");
+        //Scan for input
+        Scanner lengthInput = new Scanner(System.in);
+        //Convert input to datatype double
+        double length = lengthInput.nextDouble();
+        //Math
+        double area = (Math.sqrt(3.0)/4.0) * Math.pow(length, 2.0);
+        double volume = area * length;
+        //Print end line
+        System.out.println("The area is: " + area);
+        System.out.println("The volume is: " + volume);
     }
 }
